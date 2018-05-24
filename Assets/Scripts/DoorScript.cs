9ae230ba996d77b8aea53f01569e122dd60e168e
shassini <s.hassini@gmx.de>
2018-05-24 17:39:41 +0200

    Door animation completed

--- Assets/Scripts/DoorScript.cs
+++ Assets/Scripts/DoorScript.cs
@@ -6,9 +6,12 @@ public class DoorScript : MonoBehaviour {
 
 	private Animator _animator;
 
+	public GameObject OpenPanel = null;
+
 	// Use this for initialization
 	void Start () {
 		_animator = GetComponent<Animator>();
+		OpenPanel.SetActive(false);
 		
 	}
 
@@ -18,14 +21,33 @@ public class DoorScript : MonoBehaviour {
 		{
 
 
-			_animator.SetBool("open", true);
+			OpenPanel.SetActive(true);
 
 		}
 	}
   
 
 	// Update is called once per frame
-	void Update () {
+	void OnTriggerExit(Collider other) {
+		_animator.SetBool("open", false);
+		OpenPanel.SetActive(false);
+
 		
+	}
+
+	private bool IsOpenPanelActive{
+		get{
+			return OpenPanel.activeInHierarchy;
+		}
+	}
+
+	private void Update()
+	{
+		if(IsOpenPanelActive){
+			if(Input.GetKeyDown(KeyCode.E)){
+				OpenPanel.SetActive(false);
+				_animator.SetBool("open", true);
+			}
+		}
 	}
 }
