922c98a4cb0ceafabca88d99d29c66364de871ab
shassini <s.hassini@gmx.de>
2018-05-28 01:33:04 +0200

    getUp-Animation completed

---
+++ Assets/Scripts/LoadLevel2.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class LoadLevel2 : MonoBehaviour
+{
+	public string levelToLoad;
+	private float timer = 8f;
+	private Text timerSeconds;
+
+    // Use this for initialization
+    void Start()
+    {
+		timerSeconds = GetComponent<Text>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+		timer -= Time.deltaTime;
+		timerSeconds.text = timer.ToString("f0");
+		if(timer <= 0){
+			Application.LoadLevel(levelToLoad);
+		}
+
+    }
+}
